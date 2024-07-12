87b19ebcc849f6d6cc812088997f7ce3655e17eb
Eman Gurung <egurung1117@gmail.com>
2024-07-12 19:31:06 -0400

    header formatted

--- src/components/dealsPage.js
+++ src/components/dealsPage.js
@@ -3,7 +3,9 @@ import React from "react";
 function DealsPage() {
 
 
-    return(<></>)
+    return(<>
+        <h1>Deals</h1>
+    </>)
 }
 
 export default DealsPage;
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,12 +1,13 @@
 import React from "react";
-
+import Header from "./header.js"
 
 function HomePage() {
 
-    const [allPizzas, setAllPizzas] = ([{name: "Pepperoni", price: "5.00"}, {name: "Cheese", price: "5.00"}, {name: "Pineapple", price: "5.00"}, {name: "Sausage", price: "$5.00"}]);
+    // const [allPizzas, setAllPizzas] = ([{name: "Pepperoni", price: "5.00"}, {name: "Cheese", price: "5.00"}, {name: "Pineapple", price: "5.00"}, {name: "Sausage", price: "$5.00"}]);
 
     return(
     <div id="homePageLayout">
+        <Header/>
         {/* Header will be inserted here */}
         <div id="homePageBanner"></div>
         <div id="homePageDealsSection"></div>
